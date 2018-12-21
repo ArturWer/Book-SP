14d3150664717cf5862cdc459654d1da6182d02f
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-21 21:28:35 +0200

    Added creating list items from hand.json

--- scripts/app.js
+++ scripts/app.js
@@ -8,5 +8,19 @@ var main = function () {
 	let para = document.querySelector('p');
 	para.textContent = `${card.rank} of ${card.suit}`;
 	});
+	$.getJSON("cards/hand.json", function (hand) {
+		var $list = $("<ul>");
+		// hand — массив, поэтому мы можем применить к нему итерационный процесс
+		// с помощью цикла forEach
+		hand.forEach(function (card) {
+		// создаем элемент списка для хранения карты
+		// и присоединяем его к списку
+		var $card = $("<li>");
+		$card.text(card.rank + " of " + card.suit);
+		$list.append($card);
+		});
+		// присоединяем список к элементу main
+		$("main").append($list);
+	});
 };
 $(document).ready(main);
