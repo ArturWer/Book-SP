5ecedf8edf6da071f9774499aab6b1c3fb9b18d8
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-21 21:10:24 +0200

    Added using jQuery and create JSON request

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,12 @@
-"use strict";
-let greetings = document.querySelector("main h1");
-greetings.textContent = "Hello, world";
+var main = function () {
+	"use strict";
+	// getJSON сразу интерпретирует JSON, поэтому
+	// нет необходимости вызывать JSON.parse
+	$.getJSON("cards/aceOfSpades.json", function (card) {
+	Глава 5. Мост
+	163
+	// вводим карту в консоль
+	console.log(card);
+	});
+};
+$(document).ready(main);
