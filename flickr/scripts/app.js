08edfe1bb59850dd268b92e8c8e1d1c5ecff85e3
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-23 21:43:13 +0200

    showing by one photo into console

--- flickr/scripts/app.js
+++ flickr/scripts/app.js
@@ -2,8 +2,9 @@ var main = function () {
 	"use strict";
 	let url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
 	$.getJSON(url, function (flickrResponse) {
-	//пока мы просто выводим ответ в консоль
-	console.log(flickrResponse);
+		flickrResponse.items.forEach(function(photo){
+			console.log(photo.media.m);
+		});
 	});
 };
 $(document).ready(main);
