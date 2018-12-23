0e946dfa41fd4347e10bf4e8ce1e1dbc0f7e8819
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-23 21:23:46 +0200

    get flickr object and show it into console

--- flickr/scripts/app.js
+++ flickr/scripts/app.js
@@ -1,1 +1,9 @@
-console.log("Hello, world");
+var main = function () {
+	"use strict";
+	let url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
+	$.getJSON(url, function (flickrResponse) {
+	//пока мы просто выводим ответ в консоль
+	console.log(flickrResponse);
+	});
+};
+$(document).ready(main);
