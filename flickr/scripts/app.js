d4eb96cca5d340dda1155baf5845d2b308fba3ec
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-27 07:26:05 +0200

    Added creating new lements IMG and LI and adding them in DOM

--- flickr/scripts/app.js
+++ flickr/scripts/app.js
@@ -1,9 +1,15 @@
 var main = function () {
 	"use strict";
 	let url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
+	let ul = document.querySelector("ul");
 	$.getJSON(url, function (flickrResponse) {
 		flickrResponse.items.forEach(function(photo){
 			console.log(photo.media.m);
+			let el = document.createElement("img");
+			el.setAttribute("src", photo.media.m);
+			let li = document.createElement("li");
+			li.appendChild(el);
+			ul.appendChild(li);
 		});
 	});
 };
