22749c9838eadaa35c70148825e5c9214904ac84
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-31 17:52:48 +0200

    Added showing new images when user search it

--- flickr/Chapter_6_flickr_exercise/scripts/main.js
+++ flickr/Chapter_6_flickr_exercise/scripts/main.js
@@ -1,14 +1,30 @@
 "use strict";
 let request = new XMLHttpRequest();
 let para = document.querySelector('p');
-let url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=fireworks&format=json&jsoncallback=?";
+let url = "";
+let btn = document.querySelector('button');
+let searchTag = document.getElementById('tag')
 
-$.getJSON(url, (data)=>{
-	let imgs = data.items;
-	imgs.forEach((img)=>{
-		console.log(img.media);
-		let photo = document.createElement("img");
-		photo.src = img.media.m;
-		para.appendChild(photo);
+let searchIMG = (searchText)=>{
+	para.innerHTML = "";
+	url = `http://api.flickr.com/services/feeds/photos_public.gne?tags=${searchText}&format=json&jsoncallback=?`;
+	$.getJSON(url, (data)=>{
+		let imgs = data.items;
+		imgs.forEach((img)=>{
+			console.log(img.media);
+			let photo = document.createElement("img");
+			photo.src = img.media.m;
+			para.appendChild(photo);
+		});
 	});
+};
+
+btn.addEventListener('click', (e)=>{
+	e.preventDefault();
+	let search = searchTag.value;
+	//searchTag.value = "";
+	searchIMG(search);
+	console.log(search);
 });
+
+searchIMG("dog");
