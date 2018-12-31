d123c9526f9a70a74e0befa13f8d7aa9753dd471
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-31 13:33:56 +0200

    Write code for random changing color style

--- Chapter_6_exercise/scripts/main.js
+++ Chapter_6_exercise/scripts/main.js
@@ -4,7 +4,13 @@ h1.textContent = "Hi";
 let words = ["I'm just", "want to say", "Happy New Year!!!", "My friend:D"];
 let counter = 0;
 
+function randomColor(num) {
+	return `rgb(${(Math.random()*num)}, ${(Math.random()*num)}, ${(Math.random()*num)})`;
+}
 setInterval(()=>{
+	h1.style.color = randomColor(255);
 	h1.textContent = words[counter++];
-	if (counter > words.length-1) counter = 0;
-}, 1500);
+	if (counter > words.length-1) {
+		counter = 0;
+	}
+}, 1000);
