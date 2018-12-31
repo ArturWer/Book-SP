73369cb1306b7b600480d5bacf86503e27549b93
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-31 17:24:49 +0200

    Showing photo from JSON flicr API

--- flickr/Chapter_6_flickr_exercise/scripts/main.js
+++ flickr/Chapter_6_flickr_exercise/scripts/main.js
@@ -1,1 +1,14 @@
-"use strict"
+"use strict";
+let request = new XMLHttpRequest();
+let para = document.querySelector('p');
+let url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=fireworks&format=json&jsoncallback=?";
+
+$.getJSON(url, (data)=>{
+	let imgs = data.items;
+	imgs.forEach((img)=>{
+		console.log(img.media);
+		let photo = document.createElement("img");
+		photo.src = img.media.m;
+		para.appendChild(photo);
+	});
+});
