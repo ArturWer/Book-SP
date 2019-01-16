f4ab22733be6ad53467d59fc14fad9fc601e7b6e
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-01-16 18:10:18 +0200

    Added function getRandomHand

--- Chapter_6_POKER_exercise/scripts/main.js
+++ Chapter_6_POKER_exercise/scripts/main.js
@@ -1,17 +1,33 @@
 "use strict"
 let suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
 let ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Quin", "King"];
+let hand = getRandomHand();
 
 function random(num){
   return Math.random()*num;
-}
+};
 
 function getRadomCard(){
 	let randomSuit = Math.floor(random(suits.length));
 	let randomRank = Math.floor(random(ranks.length));
-	console.log(`${suits[randomSuit]} ${ranks[randomRank]}`);
+	return {suit:suits[randomSuit], rank:ranks[randomRank]};
 };
 
+function getRandomHand(){
+	let hand = [];
+	for (var i = 0; i < 5; i++) {
+		let card = getRadomCard();
+		/* check if it's new card or no */
+		if (hand.length>0) {
+			while (hand.indexOf(card) >= 0) {
+				console.log("Need new card");
+				card = getRadomCard();
+			}
+		} 
+		hand.push(card);
+	};
+	return hand;
+};
 
-getRadomCard();
+console.table(hand);
 
