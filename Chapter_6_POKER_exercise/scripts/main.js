c0df27dc3fd49dedc1645e195b7f03de93885b00
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-01-16 17:23:29 +0200

    Added function getRadomCard

--- Chapter_6_POKER_exercise/scripts/main.js
+++ Chapter_6_POKER_exercise/scripts/main.js
@@ -1,3 +1,17 @@
 "use strict"
 let suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
-let rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Quin", "King"];
+let ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Quin", "King"];
+
+function random(num){
+  return Math.random()*num;
+}
+
+function getRadomCard(){
+	let randomSuit = Math.floor(random(suits.length));
+	let randomRank = Math.floor(random(ranks.length));
+	console.log(`${suits[randomSuit]} ${ranks[randomRank]}`);
+};
+
+
+getRadomCard();
+
